b49886be890b314d969174d25c50a60e8ab91015
YyuriD <yurid.fkg@gmail.com>
2024-04-13 21:05:50 +0300

    First commit

---
+++ src/main/java/telran/java51/StockStatDataUploaderApplication.java
@@ -0,0 +1,13 @@
+package telran.java51;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockStatDataUploaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockStatDataUploaderApplication.class, args);
+	}
+
+}
