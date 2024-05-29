eb44208df69c5bdd79b527352d15fd74835fe8b6
YyuriD <yurid.fkg@gmail.com>
2024-05-29 06:36:12 +0300

    Add test

--- src/test/java/telran/java51/StockStatAdminServiceApplicationTests.java
+++ src/test/java/telran/java51/StockStatAdminServiceApplicationTests.java
@@ -1,13 +1,27 @@
 package telran.java51;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import telran.java51.admin.dao.AdminRepository;
+import telran.java51.admin.service.AdminServiceImpl;
+
 
 @SpringBootTest
 class StockStatAdminServiceApplicationTests {
 
+	@Autowired
+	AdminServiceImpl adminServiceImpl;
+
+	@Autowired
+	AdminRepository adminRepository;
+
 	@Test
 	void contextLoads() {
+		System.out.println("test");
+		Assertions.assertEquals(adminRepository.count(),1);
 	}
 
 }
