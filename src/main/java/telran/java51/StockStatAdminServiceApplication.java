42dff8481c3b75334a7f015643d6b28e4e969e2a
YyuriD <yurid.fkg@gmail.com>
2024-06-01 20:24:38 +0300

    commit

--- src/main/java/telran/java51/StockStatAdminServiceApplication.java
+++ src/main/java/telran/java51/StockStatAdminServiceApplication.java
@@ -1,7 +1,5 @@
 package telran.java51;
 
-import java.io.IOException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
